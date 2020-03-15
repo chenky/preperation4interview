f927e07986aad805c8f248c79d18e8d40a693b55
谌启梁 <cql@chenqideMacBook-Pro.local>
2020-03-16 01:03:00 +0800

    数组随机乱序

---
+++ tencent/2020/randomArray.js
@@ -0,0 +1,24 @@
+Array.prototype.random = function () {
+  var result = [],
+    arr = this,
+    len = arr.length;
+
+  /*for (var i = 0; i < len; i++) {
+    var t = Math.floor(Math.random() * arr.length);
+    result.push(arr[t]);
+    var left = arr.slice(0, t),
+      right = arr.slice(t + 1, arr.length);
+    arr = left;
+    arr.push.apply(arr, right);
+  }*/
+  for (var i = 0; i < len; i++) {
+    var t = Math.floor(Math.random() * arr.length);
+    result.push(arr.splice(t, 1)[0]);
+  }
+
+  return result;
+};
+
+// 比如可以应用到随机播放音乐
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(arr.random())
