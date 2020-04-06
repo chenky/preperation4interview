d386be545857e1698b1befab7015187c641154c0
谌启梁 <cql@chenqideMacBook-Pro.local>
2020-04-06 19:07:50 +0800

    快速排序思路求数组中第k大的值

--- tencent/2020/kthSmallest.js
+++ tencent/2020/kthSmallest.js
@@ -1,50 +1,35 @@
 /*
-  二叉搜索树中第K小的元素, 二叉搜索树的特点是，从小到大 left node right
-  怎么遍历树：
-
-深度优先搜索（DFS）
-在这个策略中，我们从根延伸到某一片叶子，然后再返回另一个分支。根据根节点，左节点，右节点的相对顺序，DFS 还可以分为前序，中序，后序。
-
-广度优先搜索（BFS）
-在这个策略中，我们逐层，从上到下扫描整个树。
-
-  reference： https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/solution/er-cha-sou-suo-shu-zhong-di-kxiao-de-yuan-su-by-le/
+  核心思想是每次都要先找一个中枢点 Pivot，然后遍历其他所有的数字，像这道题从大往小排的话，
+  就把大于中枢点的数字放到左半边，把小于中枢点的放在右半边，这样中枢点是整个数组中第几大的数字就确定了，
+  虽然左右两部分各自不一定是完全有序的，但是并不影响本题要求的结果，因为左半部分的所有值都大于右半部分的任意值，
+  所以我们求出中枢点的位置，如果正好是 k-1，那么直接返回该位置上的数字；如果大于 k-1，
+  说明要求的数字在左半部分，更新右边界，再求新的中枢点位置；反之则更新右半部分，
+  求中枢点的位置；不得不说，这个思路真的是巧妙啊～
+  reference: https://www.cnblogs.com/grandyang/p/4539757.html
 */
-
-/*
-通过构造 BST 的中序遍历序列，则第 k-1 个元素就是第 k 小的元素。
-  复杂度分析
-时间复杂度：O(N)O(N)，遍历了整个树。
-空间复杂度：O(N)O(N)，用了一个数组存储中序序列。
-*/ 
-function inOrder(root, arr=[]){
-  if(!root) return arr;
-  inOrder(root.left, arr);
-  arr.push(root.value);
-  inOrder(root.right, arr);
-  return arr;
+function partition(nums, left, right){
+  let pivot = nums[left], l = left + 1, r = right;
+  while (l <= r) {
+      if (nums[l] < pivot && nums[r] > pivot) {          
+        [nums[l], nums[r]] = [nums[r], nums[l]];
+        ++l;
+        --r;
+      }
+      if (nums[l] >= pivot) ++l;
+      if (nums[r] <= pivot) --r;
+  }
+  [nums[left], nums[r]] = [nums[r], nums[left]];
+  return r;
 }
-function kthSmallest(root, k){
-  let arr = inOrder(root, []);
-  return arr[k-1];
-}
-
-/*
-在栈的帮助下，可以将方法一的递归转换为迭代，这样可以加快速度，因为这样可以不用遍历整个树，可以在找到答案后停止。
-时间复杂度：\mathcal{O}(H + k)O(H+k)，其中 HH 指的是树的高度，由于我们开始遍历之前，要先向下达到叶，当树是一个平衡树时：复杂度为 \mathcal{O}(\log N + k)O(logN+k)。当树是一个不平衡树时：复杂度为 \mathcal{O}(N + k)O(N+k)，此时所有的节点都在左子树。
-空间复杂度：\mathcal{O}(H + k)O(H+k)。当树是一个平衡树时：\mathcal{O}(\log N + k)O(logN+k)。当树是一个非平衡树时：\mathcal{O}(N + k)O(N+k)。
-*/ 
-function kthSmallest2(root, k){
-  let stack=[];
+function kthSmallest3(nums, k){
+  let left = 0, right = nums.length - 1;
   while (true) {
-    while (root) {
-      stack.push(root);
-      root = root.left;
-    }    
-    root = stack.pop();
-    if(--k===0){
-      return root.value;
-    }
-    root = root.right;
+      const pos = partition(nums, left, right);
+      if (pos == k - 1) return nums[pos];
+      if (pos > k - 1) right = pos - 1;
+      else left = pos + 1;
   }
 }
+console.log(kthSmallest3([3,2,1,5,6,4], 2));
+console.log(kthSmallest3([3,2,3,1,2,4,5,5,6], 4));
+
