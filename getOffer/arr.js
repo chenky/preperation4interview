fffa283090eb8b14f48dd36cb353d486372cd1cf
chenqiliang <chenqiliang@hdsc.com>
2021-06-06 09:51:36 +0800

    数组中重复的树，范围0-(n-1),找到即返回

---
+++ getOffer/arr.js
@@ -0,0 +1,22 @@
+/**
+ * 数组相关的题目 *
+ */
+
+/**
+ * 数组中重复的树，范围0-(n-1),找到即返回
+ */
+function getRepeatInArr(arr) {
+  // 第一种方式；使用hash
+  // 第二种方式：因为范围是0-(n-1)=>num和index肯定对应
+  for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    if (element !== index) {
+      if (element !== arr[element]) {
+        [arr[index], arr[arr[index]]] = [arr[arr[index]], arr[index]];
+      } else {
+        return element;
+      }
+    }
+  }
+  return -1;
+}
